6cdd66460739f8f68381b4262866bdd98c9fc104
prachishah2003 <prachi.shah0308@gmail.com>
2025-03-17 21:09:39 +0530

    Delete lms-backend/src directory

--- lms-backend/src/main/java/com/ibizabroker/lms/dao/BooksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibizabroker.lms.dao;
-
-import com.ibizabroker.lms.entity.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksRepository extends JpaRepository<Books, Integer> {
-}
