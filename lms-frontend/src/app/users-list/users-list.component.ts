2da3f35f45c6c42d3e45b7a5370bd4007d296734
prachishah2003 <prachi.shah0308@gmail.com>
2025-03-25 00:09:46 +0530

    Add files via upload

---
+++ lms-frontend/src/app/users-list/users-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Users } from '../_model/users';
+import { UsersService } from '../_service/users.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+    selector: 'app-users-list',
+    templateUrl: './users-list.component.html',
+    styleUrls: ['./users-list.component.css'],
+    standalone: false
+})
+export class UsersListComponent implements OnInit {
+
+  users: Users[];
+
+  constructor(private usersService: UsersService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+    // this.users = [{
+    //   "userId": 1,
+    //   "name": "tarun",
+    //   "username": "tarungowda",
+    //   "role": "STUDENT",
+    //   "password": "sdklfjlakdsf"
+    // }]
+  }
+
+  private getUsers() {
+    this.usersService.getUsersList().subscribe(data =>{
+      this.users = data;
+      console.log(this.users);
+    });
+  }
+
+  userDetails(userId: number) {
+    this.router.navigate(['user-details', userId ]);
+  }
+
+  updateUser(userId: number) {
+    this.router.navigate(['update-user', userId ]);
+  }
+  deleteUser(userId: number) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.usersService.deleteUser(userId).subscribe(
+        () => {
+          alert('User deleted successfully');
+          this.getUsers(); // Refresh the user list after deletion
+        },
+        (error: HttpErrorResponse) => {
+          alert('User deleted successfully');
+          this.getUsers();  // Show error message if user has borrowed books
+        }
+      );
+    }
+  }
+
+}
