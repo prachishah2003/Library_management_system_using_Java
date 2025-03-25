50cefc9e76f98188a56394a8e6b069f69e15b1cb
prachishah2003 <prachi.shah0308@gmail.com>
2025-03-25 18:13:44 +0530

    Delete lms-frontend/src/app directory

--- lms-frontend/src/app/_model/borrow.ts
+++
@@ -1,9 +0,0 @@
-export class Borrow {
-    borrowId: number;
-    bookId: number;
-    userId: number;
-    issueDate: Date;
-    returnDate: Date;
-    dueDate: Date;
-    returnRequestStatus: 'NONE' |'PENDING' | 'APPROVED' | 'REJECTED';
-}
